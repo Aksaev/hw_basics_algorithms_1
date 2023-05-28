ca18fbc418ddce63e8aa178fc0ce7d236b69674a
Aksaev <mail-ax@ya.ru>
2023-05-28 12:48:19 +0400

    Initial commit

---
+++ src/main/java/me/aksaev/hw_basics_algorithms_1/HwBasicsAlgorithms1Application.java
@@ -0,0 +1,13 @@
+package me.aksaev.hw_basics_algorithms_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwBasicsAlgorithms1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwBasicsAlgorithms1Application.class, args);
+    }
+
+}
---
+++ src/test/java/me/aksaev/hw_basics_algorithms_1/HwBasicsAlgorithms1ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.aksaev.hw_basics_algorithms_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwBasicsAlgorithms1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
